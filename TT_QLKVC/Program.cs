ed51e65bcfa3adcd78fb0e518aab8238ca9a2402
LeThanh <LeThanh@DESKTOP-7HIL2OS>
2021-03-28 16:50:44 +0700

    up

--- TT_QLKVC/Program.cs
+++ TT_QLKVC/Program.cs
@@ -16,7 +16,7 @@ namespace TT_QLKVC
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new fQuanLyNhanVien());
+            Application.Run(new Manager());
         }
     }
 }
