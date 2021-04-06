c518e4e1f4c3110f6766f5843a0e0f82fde35e95
sonismyname <79237309+sonismyname@users.noreply.github.com>
2021-04-06 09:55:30 +0700

    S

---
+++ TT_QLKVC/DAO/DataProvider.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TT_QLKVC.DAO
+{
+    public class DataProvider
+    {
+        private static DataProvider instance;
+        public static DataProvider Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new DataProvider();
+                }
+                return DataProvider.instance;
+            }
+            private set => instance = value;
+        }
+
+        private DataProvider() { }
+
+        //private string connectionString = @"Data Source=localhost\SQLEXPRESS;Initial Catalog=KHUVUICHOIGIAITRI;Integrated Security=True";
+        private string connectionString = @"Data Source=localhost;Initial Catalog=KHUVUICHOIGIAITRI;Integrated Security=True";
+
+        #region ExcuteQuery
+        public DataTable ExecuteQuery(string query, object[] parameter = null)
+        {
+            DataTable data = new DataTable();
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+
+                connection.Open();
+
+                SqlCommand command = new SqlCommand(query, connection);
+
+                if (parameter != null)
+                {
+                    string[] temp = query.Split(' ');
+                    List<string> listPara = new List<string>();
+                    foreach (string item in temp)
+                    {
+                        if (item[0] == '@')
+                            listPara.Add(item);
+                    }
+                    for (int i = 0; i < parameter.Length; i++)
+                    {
+                        command.Parameters.AddWithValue(listPara[i], parameter[i]);
+                    }
+                }
+                SqlDataAdapter adapter = new SqlDataAdapter(command);
+                try { adapter.Fill(data); }
+                catch { }
+
+
+                connection.Close();
+            }
+            return data;
+        }
+        #endregion
+        #region ExcuteNonQuery
+        public int ExecuteNonQuery(string query, object[] parameter = null)
+        {
+            int acceptedRows = 0;
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+
+                connection.Open();
+
+                SqlCommand command = new SqlCommand(query, connection);
+
+                if (parameter != null)
+                {
+                    string[] temp = query.Split(' ');
+                    List<string> listPara = new List<string>();
+                    foreach (string item in temp)
+                    {
+                        if (item[0] == '@')
+                            listPara.Add(item);
+                    }
+                    for (int i = 0; i < parameter.Length; i++)
+                    {
+                        command.Parameters.AddWithValue(listPara[i], parameter[i]);
+                    }
+                }
+                //thực thi câu query chả về số dòng câu truy vấn thực hiện được
+                acceptedRows = -1;
+                try
+                {
+                    acceptedRows = command.ExecuteNonQuery();
+                }
+                catch { }
+                connection.Close();
+            }
+
+            return acceptedRows;
+        }
+        #endregion
+        #region ExecuteScalar
+        public object ExecuteScalar(string query, object[] parameter = null)
+        {
+            object data = 0;
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+
+                connection.Open();
+
+                SqlCommand command = new SqlCommand(query, connection);
+
+                if (parameter != null)
+                {
+                    string[] temp = query.Split(' ');
+                    List<string> listPara = new List<string>();
+                    foreach (string item in temp)
+                    {
+                        if (item[0] == '@')
+                            listPara.Add(item);
+                    }
+                    for (int i = 0; i < parameter.Length; i++)
+                    {
+                        command.Parameters.AddWithValue(listPara[i], parameter[i]);
+                    }
+                }
+                //thực thi câu query chả về số dòng câu truy vấn thực hiện được
+                try
+                {
+                    data = command.ExecuteScalar();
+                }
+                catch { }
+                connection.Close();
+            }
+
+            return data;
+        }
+        #endregion
+    }
+}
--- TT_QLKVC/fQuanLyTroChoi.cs
+++ TT_QLKVC/fQuanLyTroChoi.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using TT_QLKVC.DAO;
 
 namespace TT_QLKVC
 {
@@ -15,6 +16,53 @@ namespace TT_QLKVC
         public fQuanLyTroChoi()
         {
             InitializeComponent();
+            load();
+        }
+        #region Load
+        void load()
+        {
+            groupBox3.Visible = false;
+            textBox1.Visible = false;
+            DataTable data = new DataTable();
+            data = DataProvider.Instance.ExecuteQuery("Select makhu from khuvuichoi");
+            foreach (DataRow item in data.Rows)
+            {
+                comboBox1.Items.Add(item["makhu"]);
+            }
+        }
+        #endregion
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = DataProvider.Instance.ExecuteQuery("Select * from trochoi");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            groupBox3.Visible = true;
+
+
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            groupBox3.Visible = false;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if(textBox2.Text!="" && comboBox1.Text!="")
+                {
+                    string que = "themTC";
+                    int rez = DataProvider.Instance.ExecuteNonQuery(que, new object[] { "" + textBox2.Text + "", "" + comboBox1.Text + "" });
+                }    
+                
+            }catch(Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
         }
     }
 }
