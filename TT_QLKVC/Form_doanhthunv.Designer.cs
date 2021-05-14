731089ca0fae282b2be1e763d0c4f91097f33d8d
minhthanh120 <minhthanh1200@hotmail.com>
2021-05-14 20:13:47 +0700

    d

---
+++ TT_QLKVC/Form_doanhthunv.Designer.cs
@@ -0,0 +1,239 @@
+﻿
+namespace TT_QLKVC
+{
+    partial class Form_doanhthunv
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dateTimePicker_t_e = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker_t_s = new System.Windows.Forms.DateTimePicker();
+            this.button1 = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.tabControl1.Location = new System.Drawing.Point(0, 64);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(729, 467);
+            this.tabControl1.TabIndex = 1;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.dataGridView1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(721, 441);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Bảng";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(3, 3);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(715, 435);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.chart1);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(721, 441);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Biểu đồ";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // chart1
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
+            this.chart1.Dock = System.Windows.Forms.DockStyle.Fill;
+            legend2.Name = "Legend1";
+            this.chart1.Legends.Add(legend2);
+            this.chart1.Location = new System.Drawing.Point(3, 3);
+            this.chart1.Name = "chart1";
+            series2.ChartArea = "ChartArea1";
+            series2.Legend = "Legend1";
+            series2.Name = "Series1";
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(715, 435);
+            this.chart1.TabIndex = 7;
+            this.chart1.Text = "chart1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(326, 44);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(83, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Tháng kết thúc:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(326, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Tháng bắt đầu:";
+            // 
+            // dateTimePicker_t_e
+            // 
+            this.dateTimePicker_t_e.CustomFormat = "MM/yyyy";
+            this.dateTimePicker_t_e.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePicker_t_e.Location = new System.Drawing.Point(413, 38);
+            this.dateTimePicker_t_e.MinDate = new System.DateTime(2000, 1, 1, 0, 0, 0, 0);
+            this.dateTimePicker_t_e.Name = "dateTimePicker_t_e";
+            this.dateTimePicker_t_e.Size = new System.Drawing.Size(84, 20);
+            this.dateTimePicker_t_e.TabIndex = 8;
+            this.dateTimePicker_t_e.ValueChanged += new System.EventHandler(this.dateTimePicker_t_e_ValueChanged);
+            // 
+            // dateTimePicker_t_s
+            // 
+            this.dateTimePicker_t_s.CustomFormat = "MM/yyyy";
+            this.dateTimePicker_t_s.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePicker_t_s.Location = new System.Drawing.Point(413, 12);
+            this.dateTimePicker_t_s.MinDate = new System.DateTime(2000, 1, 1, 0, 0, 0, 0);
+            this.dateTimePicker_t_s.Name = "dateTimePicker_t_s";
+            this.dateTimePicker_t_s.Size = new System.Drawing.Size(84, 20);
+            this.dateTimePicker_t_s.TabIndex = 7;
+            this.dateTimePicker_t_s.Value = new System.DateTime(2021, 5, 1, 0, 0, 0, 0);
+            this.dateTimePicker_t_s.ValueChanged += new System.EventHandler(this.dateTimePicker_t_s_ValueChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(587, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Thống kê";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(226, 12);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(89, 17);
+            this.radioButton1.TabIndex = 11;
+            this.radioButton1.Text = "Theo 1 tháng";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Checked = true;
+            this.radioButton2.Location = new System.Drawing.Point(226, 34);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(73, 17);
+            this.radioButton2.TabIndex = 12;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Theo năm";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(77, 15);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 13;
+            // 
+            // Form_doanhthunv
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(729, 531);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dateTimePicker_t_e);
+            this.Controls.Add(this.dateTimePicker_t_s);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form_doanhthunv";
+            this.Text = "Doanh thu nhân viên";
+            this.Load += new System.EventHandler(this.Form_doanhthunv_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.tabPage2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker_t_e;
+        private System.Windows.Forms.DateTimePicker dateTimePicker_t_s;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
---
+++ TT_QLKVC/Form_doanhthunv.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace TT_QLKVC
+{
+    public partial class Form_doanhthunv : Form
+    {
+        public Form_doanhthunv()
+        {
+            InitializeComponent();
+        }
+        string constr = @"Data Source=localhost\SQLEXPRESS;Initial Catalog=KHUVUICHOIGIAITRI;Integrated Security=True";
+        string[] type = {"Tổng doanh thu","Doanh thu bán vé", "Doanh thu dịch vụ" };
+
+        private void Form_doanhthunv_Load(object sender, EventArgs e)
+        {
+            comboBox1.Text = type[0];
+            for(int i=0; i<type.Length; i++){
+                comboBox1.Items.Add(type[i]);
+            }
+            dateTimePicker_t_s.MaxDate = DateTime.Today;
+            dateTimePicker_t_e.MaxDate = DateTime.Today;
+            //this.dateTimePicker_t_e.Da dateTimePicker_t_e.
+        }
+        void change()
+        {
+            if(radioButton1.Checked==true)
+            {
+                dateTimePicker_t_e.Enabled = false;
+                dateTimePicker_t_e.Visible = false;
+                label1.Text = "Tháng:";
+                label2.Visible = false;
+                label1.Location = new Point(365, 14);
+            }
+            else
+            {
+                label2.Visible = true;
+                label1.Text = "Tháng bắt đầu:";
+                label1.Location = new Point(326,14);
+                dateTimePicker_t_e.Enabled = true;
+                dateTimePicker_t_e.Visible = true;
+            }    
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            change();
+        }
+
+        private void dateTimePicker_t_s_ValueChanged(object sender, EventArgs e)
+        {
+            //if(dateTimePicker_t_s.Value> dateTimePicker_t_e.Value)
+            //{
+               // MessageBox.Show("Thời gian bắt đầu thống kê không được lớn hơn Thời gian kết thúc thống kê");
+                //dateTimePicker_t_s.Value = dateTimePicker_t_e.Value;
+                dateTimePicker_t_e.MinDate = dateTimePicker_t_s.Value;
+                //label3.Text = dateTimePicker_t_s.Value.ToString("MM") + "-30-" + dateTimePicker_t_s.Value.ToString("yyyy");
+            //}    
+        }
+
+        private void dateTimePicker_t_e_ValueChanged(object sender, EventArgs e)
+        {
+            //if (dateTimePicker_t_s.Value > dateTimePicker_t_e.Value)
+            //{
+                //MessageBox.Show("Thời gian bắt đầu thống kê không được lớn hơn Thời gian kết thúc thống kê");
+                dateTimePicker_t_s.Value = dateTimePicker_t_e.Value;
+            //label3.Text = cvdt(dateTimePicker_t_s.Value.ToString("yyyy"));
+            //}
+        }
+
+        //string cvdt(string d)
+        //{
+        //    //DateTime dateTime = Convert.ToDateTime(d);
+        //   // d = dateTime.ToString("MM-dd-yyyy");
+        //    return d;
+        //}
+        //string start = ""; string end = "";
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string start=""; string end="";
+            if(radioButton1.Checked==true)
+            {
+                
+                if (Convert.ToInt32(dateTimePicker_t_s.Value.ToString("MM")) == 1 || Convert.ToInt32(dateTimePicker_t_s.Value.ToString("MM")) == 3 || Convert.ToInt32(dateTimePicker_t_s.Value.ToString("MM")) == 5 || Convert.ToInt32(dateTimePicker_t_s.Value.ToString("MM")) == 7 || Convert.ToInt32(dateTimePicker_t_s.Value.ToString("MM")) == 8 || Convert.ToInt32(dateTimePicker_t_s.Value.ToString("MM")) == 10 || Convert.ToInt32(dateTimePicker_t_s.Value.ToString("MM")) == 12)
+                {
+                    end = dateTimePicker_t_s.Value.ToString("MM") + "-31-" + dateTimePicker_t_s.Value.ToString("yyyy");
+                }
+                else if (Convert.ToInt32(dateTimePicker_t_s.Value.ToString("MM")) == 2)
+                {
+                    end = dateTimePicker_t_s.Value.ToString("MM")+"-28-" + dateTimePicker_t_s.Value.ToString("yyyy");
+                }
+                else
+                {
+                    end = dateTimePicker_t_s.Value.ToString("MM") + "-30-" + dateTimePicker_t_s.Value.ToString("yyyy");
+                }
+
+                
+
+            }
+            else
+            {
+                if (Convert.ToInt32(dateTimePicker_t_e.Value.ToString("MM")) == 1 || Convert.ToInt32(dateTimePicker_t_e.Value.ToString("MM")) == 3 || Convert.ToInt32(dateTimePicker_t_e.Value.ToString("MM")) == 5 || Convert.ToInt32(dateTimePicker_t_e.Value.ToString("MM")) == 7 || Convert.ToInt32(dateTimePicker_t_e.Value.ToString("MM")) == 8 || Convert.ToInt32(dateTimePicker_t_e.Value.ToString("MM")) == 10 || Convert.ToInt32(dateTimePicker_t_e.Value.ToString("MM")) == 12)
+                {
+                    end = dateTimePicker_t_e.Value.ToString("MM") + "-31-" + dateTimePicker_t_e.Value.ToString("yyyy");
+                }
+                else if (Convert.ToInt32(dateTimePicker_t_e.Value.ToString("MM")) == 2)
+                {
+                    end = dateTimePicker_t_e.Value.ToString("MM") + "-28-" + dateTimePicker_t_e.Value.ToString("yyyy");
+                }
+                else
+                {
+                    end = dateTimePicker_t_e.Value.ToString("MM") + "-30-" + dateTimePicker_t_e.Value.ToString("yyyy");
+                }
+                //dateTimePicker_t_s.Value.ToString("MM-yyyy");
+            }
+            start = dateTimePicker_t_s.Value.ToString("MM") + "-01-" + dateTimePicker_t_s.Value.ToString("yyyy");
+            //label3.Text = "execute tongdoanhthunv '" + start + "','" + end + "'";
+            //label4.Text = end;
+            if (comboBox1.Text==type[0])
+            {
+                using (SqlConnection sqlcon = new SqlConnection(constr))
+                {
+                    sqlcon.Open();
+                    SqlDataAdapter sqlData = new SqlDataAdapter("EXECUTE tongdoanhthunv '" + start + "','" + end + "'", sqlcon);
+                    DataTable dataTable = new DataTable();
+                    sqlData.Fill(dataTable);
+                    dataGridView1.DataSource = dataTable;
+                    MessageBox.Show("EXECUTE tongdoanhthunv '" + start + "','" + end + "");
+                }
+            }
+            else if(comboBox1.Text==type[1])
+            {
+
+            }   
+            else if(comboBox1.Text==type[2])
+            {
+
+            }
+
+            private DataTable loadchart()
+            {
+
+            }
+
+
+        }
+    }
+}
